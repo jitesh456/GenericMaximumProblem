dc8af67f3f7da38cc1b9484105fda3a8c0b989f2
jitesh <jitesh0dubey@gmail.com>
2020-02-28 17:01:13 +0530

    REFACTORED:used generic method for all data type

--- src/main/java/com/generic/maximum/GenericMaximumProblem.java
+++ src/main/java/com/generic/maximum/GenericMaximumProblem.java
@@ -1,10 +1,10 @@
 package com.generic.maximum;
 
 public class GenericMaximumProblem {
-    public Integer findMaximum(Integer firstValue, Integer secondValue,Integer thirdValue) {
-        Integer maximumValue=firstValue;
+    public <E extends  Comparable<E>> E findMaximum(E firstValue, E secondValue,E thirdValue) {
+        E maximumValue=firstValue;
 
-        if(firstValue.compareTo(secondValue)>0)
+        if(firstValue.compareTo( secondValue)>0)
             maximumValue=firstValue;
 
         if(secondValue.compareTo(firstValue)>0)
@@ -16,34 +16,4 @@ public class GenericMaximumProblem {
         return  maximumValue;
     }
 
-
-    public Float findMaximum(Float firstValue, Float secondValue, Float thirdValue) {
-       Float maximumValue=firstValue;
-
-        if(firstValue.compareTo(secondValue)>0)
-            maximumValue=firstValue;
-
-        if(secondValue.compareTo(firstValue)>0)
-            maximumValue=secondValue;
-
-        if(thirdValue.compareTo(maximumValue)>0)
-            maximumValue=thirdValue;
-
-        return  maximumValue;
-    }
-
-    public String findMaximum(String firstValue, String secondValue, String thirdValue) {
-        String maximumValue=firstValue;
-
-        if(firstValue.compareTo(secondValue)>0)
-            maximumValue=firstValue;
-
-        if(secondValue.compareTo(firstValue)>0)
-            maximumValue=secondValue;
-
-        if(thirdValue.compareTo(maximumValue)>0)
-            maximumValue=thirdValue;
-
-        return  maximumValue;
-    }
 }
